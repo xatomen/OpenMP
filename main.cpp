6c5f9054a58747f5b3fbde23058579a1a296335f
xatomen <jorgito899@gmail.com>
2024-06-06 22:19:23 -0400

    Se agrega el caso parallel con sección crítica, y el parallel for

--- main.cpp
+++ main.cpp
@@ -2,10 +2,16 @@
 #include <omp.h>
 
 void parallel_sin_seccion_critica();
+void parallel_con_seccion_critica();
+void parallel_for();
 
 int main() {
 
     parallel_sin_seccion_critica();
+
+    parallel_con_seccion_critica();
+
+    parallel_for();
 
 }
 
@@ -24,5 +30,39 @@ void parallel_sin_seccion_critica(){
             j++;
         }
     }
+    std::cout << "valor j = " << j << std::endl;
+}
+
+void parallel_con_seccion_critica(){
+    int j = 0;
+    /*
+     * A diferencia del caso anterior, sí se considera sección crítica, por lo que no habrá incoherencia, sin embargo,
+     * se observa que, cómo el parallel ejecuta la sentencia con cada uno de sus hilos, en mi caso 8 hilos, el resultado es 80.
+    */
+#pragma omp parallel
+    {
+        for(int i=0; i<10; i++){
+#pragma omp critical
+            {
+                j++;
+            }
+        }
+    }
+    std::cout << "valor j = " << j << std::endl;
+}
+
+void parallel_for(){
+    int j = 0;
+    /*
+     * Cuando tenemos un for, para evitar problemas como el anterior, debemos utilizar un "parallel for", en donde
+     * tenemos que escribir "#pragma parallel for" encima del for correspondiente, y debemos quitar los corchetes.
+    */
+#pragma omp parallel for
+        for(int i=0; i<10; i++) {
+#pragma omp critical
+            {
+                j++;
+            }
+        }
     std::cout << "valor j = " << j << std::endl;
 }
